63ceaca23bfc3ded63ea587f4b309110cd2faa60
DanielLee <ggguest329@gmail.com>
2018-08-25 17:22:26 +0800

    Version 2: Setting up the Methods(Simply display the score of the button being pressed without accumulating it)

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.android.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void displayForTeamA(int score) {
+        TextView teamATextView = findViewById(R.id.team_a_score);
+        teamATextView.setText(String.valueOf(score));
+//        teamATextView.setText("" + score);   // equivalent way to setText() an integer
+    }
+
+    public void add_3_points(View view){
+        displayForTeamA(3);
+    }
+
+    public void add_2_points(View view){
+        displayForTeamA(2);
+    }
+
+    public void add_1_point(View view){
+        displayForTeamA(1);
+    }
+
 }
